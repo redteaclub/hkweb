cdac710e3bf0050bf903ca3324f9774f6e54c6c8
redteaclub <redteaclub@gmail.com>
2018-09-07 16:03:24 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  title: 'HKWeb',
+  description: 'HKWeb',
+  themeConfig: {
+    docsDir: 'docs',
+    repo: 'redteaclub/hkweb',
+    sidebar: [
+      '/',
+      '/welcome',
+      '/test'
+    ],
+    nav: [
+      {
+        text: 'Admin',
+        link: '/admin/#/',
+      }
+    ]
+  }
+}
